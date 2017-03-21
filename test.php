c40b6ac35da8f0ccb244ce21ec7d00c5e7e75f24
Riccardo Gialdi <gialduz@gmail.com>
2017-03-21 18:11:27 +0100

    Evento-Luogo
    motore PHP recupera tabelle Evento, Luogo e relazione, fa il jion e
    restituisce qualcosa di quasi leggibile

---
+++ test.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "mysql";
+    $dbname = "segni";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    
+    //TABELLA LUOGO
+    $sql = "SELECT id, nome, tipoVia, via FROM Luogo WHERE 1";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Nome: " . $row["nome"]. " -> " . $row["tipoVia"]. " " .$row["via"] .  "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    echo "<br><br>";
+    
+    //TABELLA EVENTO
+    $sql = "SELECT * FROM Evento WHERE 1";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Nome: " . $row["nome"]. " Durata: " . $row["durata"]. " PAGAMENTO: " .$row["ticket"] .  "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    
+    //TABELLA istanzeEvento
+    echo "<br><br><h2>EventoLuogoData</h2>";
+    
+    $sqlTabellaIstanzeEventi=   "SELECT E.nome AS Evento, eld.data AS data, eld.orario AS orario, L.nome AS dove, eld.speciale AS speciale FROM ((Evento AS E INNER JOIN eventoLuogoData AS eld ON E.id = eld.id_evento) INNER JOIN Luogo AS L ON L.id = eld.id_luogo)  ORDER BY data, orario, Evento;";
+    
+    $result = $conn->query($sqlTabellaIstanzeEventi);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if ($row["speciale"]) {echo "<b> SPECIALE</b>";}
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    $conn->close();
+    ?>
+</body>
+
+</html>
